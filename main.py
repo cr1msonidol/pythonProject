40bf838417c05758ab07dd51ae486efae4c8c473
Andrey Oleynikov <oleinikov.2000.ao@gmail.com>
2021-02-21 22:08:10 +0600

    add blinking shrug emoji

--- main.py
+++ main.py
@@ -4,7 +4,27 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    return """
+<style>
+.blinking{
+
+font-size: 100pt;
+font-style: bold;
+    animation:blinkingText 1.2s infinite;
+}
+@keyframes blinkingText{
+    0%{     color: #000;    }
+    49%{    color: #000; }
+    60%{    color: transparent; }
+    99%{    color:transparent;  }
+    100%{   color: #000;    }
+}
+</style>    
+    <span class="blinking">¯\_(ツ)_/¯</span>
+
+    
+    
+    """
 
 if __name__ == '__main__':
     app.run()
